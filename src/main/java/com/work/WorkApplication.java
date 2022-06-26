8c77af3b9d6ca48d0c86407c719cfa5ebea006b2
phannam1 <35187952+phannam1@users.noreply.github.com>
2022-06-26 23:03:55 +0700

    fix deploy tomcat

--- src/main/java/com/work/WorkApplication.java
+++ src/main/java/com/work/WorkApplication.java
@@ -2,9 +2,10 @@ package com.work;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WorkApplication {
+public class WorkApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkApplication.class, args);
