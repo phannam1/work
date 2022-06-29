1d6cbf7a8e71011a93cb4e3fd2d480ceca2f13f2
phannam1 <namphan0952@gmail.com>
2022-06-29 13:01:18 +0700

    Spring Sercurity

---
+++ src/main/java/com/work/login/LoginRequest.java
@@ -0,0 +1,9 @@
+package com.work.login;
+
+import com.work.common.AbstractRequest;
+
+public class LoginRequest extends AbstractRequest {
+
+	public String username;
+	public String password;
+}
---
+++ src/main/java/com/work/login/LoginResponse.java
@@ -0,0 +1,7 @@
+package com.work.login;
+
+import com.work.common.AbstractResponse;
+
+public class LoginResponse extends AbstractResponse{
+
+}
